6c4eba27968299b11413e95ca77f91dcc5424077
Shubham-Rasal <bluequbits@gmail.com>
2023-05-10 22:12:21 +0530

    nextjs update to 13.4.0

--- fundapp/next.config.js
+++ fundapp/next.config.js
@@ -1,27 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages : ['bcrypt']
-  },
+  // experimental: {
+  //   appDir: true,
+  // },
   env: {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  },
-  api : {
-    bodyParser: true,
-    externalResolver: true,
+  }, 
+  typescript: {
+    ignoreBuildErrors: true,
   },
 
-  webpack5: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      }
-    }
-    return config;
-  },
+ 
 }
 
 module.exports = nextConfig

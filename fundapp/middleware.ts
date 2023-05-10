d2f76ace9b80927060fb10cb8e3dccbf4ad30a60
Shubham-Rasal <bluequbits@gmail.com>
2023-05-10 22:11:33 +0530

    middleware update

--- fundapp/middleware.ts
+++ fundapp/middleware.ts
@@ -2,27 +2,28 @@ import { NextRequest, NextResponse } from "next/server";
 import { verify } from "@/lib/auth";
 
 export async function middleware(request: NextRequest) {
-  const cookies = request.cookies.getAll();
-  console.log("cookies", cookies);
   const jwt = request.cookies.get("jwt")?.value;
 
-  if (!jwt) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
   const verified = await verify(jwt).catch((e) => {
-    console.log("error", e);
+    console.log("error in verifying ", e);
   });
+
+  console.log("verified", verified);  
+
+  
 
   if (!verified && request.nextUrl.pathname === "/profile") {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+
+ 
 
   if (verified && request.nextUrl.pathname === "/login") {
     return NextResponse.redirect(new URL("/profile", request.url));
   }
 
   return NextResponse.next();
+  
 }
 
 export const config = {

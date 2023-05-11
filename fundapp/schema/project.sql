6cda8c4980bfb49aca51ded554ed38c9146bdcb2
Shubham-Rasal <bluequbits@gmail.com>
2023-05-12 00:03:08 +0530

    schema changes

--- fundapp/schema/project.sql
+++ fundapp/schema/project.sql
@@ -1,13 +1,13 @@
 show tables;
 
--- create table Project(
---     name varchar(255),
---     description varchar(255),
---     funding_goal int,
---     funding_raised int,
---     id int not null auto_increment,
---     primary key(id)
--- );
+create table Project(
+    name varchar(255),
+    description varchar(255),
+    funding_goal int,
+    funding_raised int,
+    id int not null auto_increment,
+    primary key(id)
+);
 
 -- alter table Project add column project_deadline date;
 -- alter table Project add column created_at timestamp default current_timestamp;
--- fundapp/schema/user.sql
+++ fundapp/schema/user.sql
@@ -4,15 +4,15 @@
 
 -- show tables;
 
--- create table User(
---     name varchar(255),
---     email varchar(255),
---     password varchar(255),
---     id int not null auto_increment,
---     account_balance int,
---     primary key(id)
+create table User(
+    name varchar(255),
+    email varchar(255),
+    password varchar(255),
+    id int not null auto_increment,
+    account_balance int,
+    primary key(id)
 
--- );
+);
 
 -- alter table User add foreign key (id) references Project(created_by);
 

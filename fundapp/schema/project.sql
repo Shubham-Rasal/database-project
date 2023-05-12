d183c6babc8e97f0610c3faca028adcdce43fdd5
Shubham-Rasal <bluequbits@gmail.com>
2023-05-13 02:47:43 +0530

    config changes

--- fundapp/schema/project.sql
+++ fundapp/schema/project.sql
@@ -1,17 +1,22 @@
+
+use nextjs;
+
 show tables;
 
-create table Project(
-    name varchar(255),
-    description varchar(255),
-    funding_goal int,
-    funding_raised int,
-    id int not null auto_increment,
-    primary key(id)
-);
+-- create table Project(
+--     name varchar(255),
+--     description varchar(255),
+--     funding_goal int,
+--     funding_raised int,
+--     id int not null auto_increment,
+--     primary key(id)
+-- );
 
 -- alter table Project add column project_deadline date;
 -- alter table Project add column created_at timestamp default current_timestamp;
 -- alter table Project add column created_by int not null;
-alter table Project add foreign key (created_by) references User(id);
+-- alter table Project add foreign key (created_by) references User(id);
+
+alter table Project modify column description varchar(1000);
 
 desc Project;

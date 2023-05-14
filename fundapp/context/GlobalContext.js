8e8b5033cc3e4cae473d0540bcc6c695c4b81c06
Shubham-Rasal <bluequbits@gmail.com>
2023-05-14 13:09:46 +0530

    get funded projects , funders

--- fundapp/context/GlobalContext.js
+++ fundapp/context/GlobalContext.js
@@ -1,7 +1,13 @@
 "use client";
 import { createContext, useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-export const GlobalContext = createContext();
+export const GlobalContext = createContext({
+    user: null,
+    setUser: () => {},
+    projects: [],
+    setProjects: () => {},
+    loading: false,
+});
 
 async function getUser() {
   const res = await fetch("http://localhost:3000/api/profile", {
@@ -11,14 +17,14 @@ async function getUser() {
     },
     cache: "no-store",
   });
-  console.log(res);
+//   console.log(res);
   if (!res.ok) {
     throw new Error("Failed to fetch profile");
   }
 
   try {
     const profile = await res.json();
-    console.log(profile);
+    // console.log(profile);
     return profile;
   } catch (error) {
     throw new Error("Failed to parse JSON response");
@@ -28,18 +34,25 @@ async function getUser() {
 export const GlobalProvider = ({ children }) => {
   const [user, setUser] = useState({});
   const [projects, setProjects] = useState([]);
+  const [funded_projects, setFundedProjects] = useState([]);
+  const [funded_user, setFundedUser] = useState([]);
+  const [ loading , setLoading ] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
-    getUser().then(({ user, projects }) => {
+    setLoading(true);
+    getUser().then(({ user, projects , funded_projects , funded_user }) => {
       setUser(user);
       setProjects(projects);
+      setFundedProjects(funded_projects);
+      setFundedUser(funded_user);
       console.log("GlobalProvider", user);
+        setLoading(false);
     });
   }, [pathname]);
 
   return (
-    <GlobalContext.Provider value={{ user, setUser , projects , pathname }}>
+    <GlobalContext.Provider value={{ user, setUser , projects , funded_projects , funded_user , pathname , loading }}>
       {children}
     </GlobalContext.Provider>
   );

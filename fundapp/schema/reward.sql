370d642ffc75b0b0fe3d600b5002dc4bd49aa3cf
Shubham-Rasal <bluequbits@gmail.com>
2023-05-22 09:17:28 +0530

    added reward schema

---
+++ fundapp/schema/reward.sql
@@ -0,0 +1,15 @@
+-- use nextjs;
+
+-- show tables;
+
+-- create table reward(
+--     id varchar(255) primary key,
+--     name varchar(255) not null,
+--     details varchar(255) not null,
+--     funder int not null,
+--     creator int not null,
+--     foreign key (funder) references user(id) on delete cascade,
+--     foreign key (creator) references user(id) on delete cascade    
+-- );
+
+-- desc reward;
